f4ffb78897b6695aa9851b7b8fcb1dd05a848df3
daniel <daniel.enrriquesmercedes@gmail.com>
2023-06-28 12:40:38 +0200

    ft: i created the submit post images component

--- src/components/Layout/Community/NewPostForm.tsx
+++ src/components/Layout/Community/NewPostForm.tsx
@@ -1,4 +1,6 @@
+import ImageUpload from '@/components/Posts/ImageUpload';
 import TabItem  from '@/components/Posts/TabItem';
+import TextInputs from '@/components/Posts/TextInputs';
 import { Flex, Icon } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { BiPoll } from 'react-icons/bi';
@@ -46,6 +48,40 @@ export type TabItemType = {
 const NewPostForm:React.FC<NewPostFormProps> = () => {
 
     const [selectedTab, setSelectedTab] = useState(FORM_TABS[0].title);
+    const [textInput, setTextInput] = useState({
+        title: '',
+        body: '',
+    })
+    const [loading, setLoading] = useState(false)
+
+    const [selectedFile, setSelectedFile] = useState<string>();
+
+    const handleCreatePost = async () => {};
+
+    const onSelectImage =  (event: React.ChangeEvent<HTMLInputElement>) => {
+        const reader = new FileReader();
+
+        if(event.target.files?.[0]) {
+            reader.readAsDataURL(event.target.files[0]);
+
+        }
+        reader.onload = (readerEvent) => {
+            if(readerEvent.target?.result) {
+                setSelectedFile(readerEvent.target.result as string)
+            }
+        }
+    }
+
+    const onTextChange =  (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+
+        setTextInput({
+                ...textInput,
+                [event.target.name] : event.target.value,
+                
+        })
+
+
+    }
     
     return (
         <Flex
@@ -62,6 +98,7 @@ const NewPostForm:React.FC<NewPostFormProps> = () => {
                     FORM_TABS.map((item) => (
                         <>
                         <TabItem
+                            key={item.title}
                             item={item}
                             selected={item.title === selectedTab}
                             setSelectedTab={setSelectedTab}
@@ -69,6 +106,28 @@ const NewPostForm:React.FC<NewPostFormProps> = () => {
                         </>
 
                     ))
+                }
+
+            </Flex >
+            <Flex p={4}>
+                {selectedTab === 'Post' && <TextInputs
+                    textInputs={textInput}
+                    handleCreatePost={handleCreatePost}
+                    onChange={onTextChange}
+                    loading={loading}
+
+                />}
+                {
+                    selectedTab === 'Images & Video' && 
+                    <ImageUpload
+                        selectedFile={selectedFile}
+                        setSelectedFile={setSelectedFile}
+                        onSelectImage={onSelectImage}
+                        setSelectedTab={setSelectedTab}
+                        
+                    />
+                    
+
                 }
 
             </Flex>
